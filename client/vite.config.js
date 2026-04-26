6d13306b89a49a736ccd892ff5091b21a7d9d65f
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-04-26 21:13:44 +0530

    Added Docker Compose

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:5001',
+                target: process.env.VITE_PROXY_TARGET || 'http://localhost:5001',
                 changeOrigin: true,
             }
         }
