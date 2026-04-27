38f0f58fdabee496fd383a18df36f73de07489e6
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-04-27 18:01:09 +0530

    fix: prettier formatting

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,10 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5001;
 
-app.get("/", (req, res) => {
-  res.status(200).send("OK");
+app.get('/', (req, res) => {
+  res.status(200).send('OK');
 });
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
