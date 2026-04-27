832402aeaecce995ba31033a2d181f7f499c4fcb
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-04-27 17:39:36 +0530

    Updated index.js

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,10 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5001;
 
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
